b8e2ff8c9b043f81873a3bf325bb7798de3a6cf8
Zakariyya Mughal <zaki.mughal@gmail.com>
2017-08-16 21:58:11 -0500

    Add link to OpenJPEG website
    Fixes <https://github.com/project-renard/p5-Alien-OpenJPEG/issues/5>.

--- lib/Alien/OpenJPEG.pm
+++ lib/Alien/OpenJPEG.pm
@@ -7,3 +7,9 @@ use warnings;
 use parent qw(Alien::Base::Dino);
 
 1;
+__END__
+=head1 SEE ALSO
+
+L<OpenJPEG|http://www.openjpeg.org/>
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Alien-OpenJPEG/>
